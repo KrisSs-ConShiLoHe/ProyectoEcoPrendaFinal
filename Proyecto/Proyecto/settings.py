d636ab92f1d6682a610eda125407d427a8db275c
KrisSs-ConShiLoHe <solwayfirth56@gmail.com>
2025-12-09 16:28:56 -0300

    cambio 1

--- Proyecto/Proyecto/settings.py
+++ Proyecto/Proyecto/settings.py
@@ -44,8 +44,6 @@ ALLOWED_HOSTS = os.environ.get(
 
 CSRF_TRUSTED_ORIGINS = [
     'https://proyectoecoprenda-ykp.onrender.com',
-    'http://localhost:8000',
-    'http://127.0.0.1:8000',
 ]
 
 # Application definition
