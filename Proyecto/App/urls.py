680974f0834ee94730a7a847a35932263ac2bd3f
KrisSs-ConShiLoHe <solwayfirth56@gmail.com>
2025-12-16 01:12:05 -0300

    xd

--- Proyecto/App/urls.py
+++ Proyecto/App/urls.py
@@ -70,6 +70,8 @@ urlpatterns = [
     path('campanas-solidarias', views.campanas_solidarias, name='campanas_solidarias'),
     path('detalle-campana/<int:id>/', views.detalle_campana, name='detalle_campana'),
     path('donar-a-campana/<int:id>/', views.donar_a_campana, name='donar_a_campana'),
+    path('editar-campana/<int:id>/', views.editar_campana, name='editar_campana'),
+    path('eliminar-campana/<int:id>/', views.eliminar_campana, name='eliminar_campana'),
 
     # Logo de fundación
     path('fundacion/<int:id_fundacion>/actualizar-logo/', views.actualizar_logo_fundacion, name='actualizar_logo_fundacion'),
